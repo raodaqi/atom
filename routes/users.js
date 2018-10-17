857f9c0535e708e68d559624cf13d9043957027e
“寻未四叔” <598471284@qq.com>
2018-10-17 23:59:16 +0800

    更改users api

--- routes/users.js
+++ routes/users.js
@@ -118,4 +118,78 @@ router.post('/', async function(ctx) {
     }
 });
 
+// 新增 Todo 项目
+router.post('/login', async function(ctx) {
+    const phone = ctx.request.body.phone;
+    const password = ctx.request.body.password;
+
+  //判断手机号是否正确
+    var phoneReg = /(^1[3|4|5|7|8]\d{9}$)|(^09\d{8}$)/;
+
+	if (!phoneReg.test(phone)) {
+        
+        // ctx.status = 204
+        // ctx.message = "手机号不正确"
+        ctx.body = {
+            code : "1001",
+            message:"手机号不正确",
+            data:{}
+        }
+        console.log('手机号不正确')
+        return false;
+    }
+
+    if(password.length  < 6){
+        // ctx.status = 1002
+        // ctx.message = "密码太短"
+        ctx.body = {
+            code : "1002",
+            message:"密码太短",
+            data:{}
+        }
+        console.log('密码太短')
+        return false;
+    }
+
+    var query = new AV.Query('_User');
+    query.equalTo("username",phone);
+    var userQuery = await query.first();
+    console.log(userQuery)
+
+    if(!userQuery){
+        //存在
+        ctx.body = {
+            code : "1003",
+            message:"用户不存在",
+            data:{}
+        }
+        return;
+    }
+
+    //查询用户
+
+    var user = {}
+
+    try{
+        user = await AV.User.logIn(phone, password)
+    }catch(err){
+        console.log(err)
+        ctx.body = {
+            code : "1004",
+            message:"登录失败",
+            data:user
+        }
+
+        return;
+    }
+
+    console.log("user")
+    console.log(user)
+    ctx.body = {
+        code : "200",
+        message:"获取成功",
+        data:user
+    }
+});
+
 module.exports = router;
